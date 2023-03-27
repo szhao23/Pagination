4beabd3030c16e5381ea33c033cf93abf8378037
szhao23 <stevenzhao456@gmai.com>
2023-03-27 03:03:11 -0500

    adding JS code, added button id's and selected DOM elements in JS script file

---
+++ code/src/index.js
@@ -0,0 +1,16 @@
+// Select DOM elements
+const startBtn = document.querySelector("#startBtn"),
+  endBtn = document.querySelector("#endBtn"),
+  prevNext = document.querySelector("#prevNext"),
+  numbers = document.querySelector(".links");
+
+console.log(
+  "Start Button: ",
+  startBtn,
+  "End Button: ",
+  endBtn,
+  "Previous Next: ",
+  prevNext,
+  "Numbers: ",
+  numbers
+);
