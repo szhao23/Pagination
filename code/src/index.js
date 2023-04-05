79be46678603d607aadea046ef5219a3f0dc281f
szhao23 <stevenzhao456@gmai.com>
2023-04-05 03:06:49 -0500

    finalized project

--- code/src/index.js
+++ code/src/index.js
@@ -1,8 +1,8 @@
 // Select DOM elements
 const startBtn = document.querySelector("#startBtn"),
   endBtn = document.querySelector("#endBtn"),
-  prevNext = document.querySelector("#prevNext"),
-  numbers = document.querySelector(".links");
+  prevNext = document.querySelectorAll(".prevNext"),
+  numbers = document.querySelectorAll(".link");
 
 // Testing
 console.log(
@@ -22,7 +22,7 @@ let currentStep = 0;
 // Function to update button states
 const updateBtn = () => {
   // If we are currently at the last step
-  if (currentStep === 5) {
+  if (currentStep === 4) {
     endBtn.disabled = true;
     prevNext[1].disabled = true;
   } else if (currentStep === 0) {
@@ -30,7 +30,7 @@ const updateBtn = () => {
     startBtn.disabled = true;
     prevNext[0].disabled = true;
   } else {
-    endBtn.disabled = true;
+    endBtn.disabled = false;
     prevNext[1].disabled = false;
     startBtn.disabled = false;
     prevNext[0].disabled = false;
@@ -39,7 +39,7 @@ const updateBtn = () => {
 
 // Add event listeners for number links
 numbers.forEach((number, numIndex) => {
-  number.addEventListener("click", () => {
+  number.addEventListener("click", (e) => {
     e.preventDefault();
     // Set current step to the selected/clicked number link
     currentStep = numIndex;
@@ -55,3 +55,38 @@ numbers.forEach((number, numIndex) => {
 });
 
 // Add event listeners for the Previous and Next buttons
+prevNext.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // Increase or Decrease the current step based on the button/step that is clicked
+    currentStep += e.target.id === "next" ? 1 : -1;
+    numbers.forEach((number, numIndex) => {
+      // Toggle the "active" class on the number links based on the current step
+      number.classList.toggle("active", numIndex === currentStep);
+      updateBtn(); // Update button state
+    });
+  });
+});
+
+// Add event listener to the "Start Button"
+startBtn.addEventListener("click", () => {
+  // Remove the "active" class from the previously active number link
+  document.querySelector(".active").classList.remove("active");
+  // Add the "active" class to the first number link
+  numbers[0].classList.add("active");
+  currentStep = 0;
+  updateBtn(); // Update button state
+  endBtn.disabled = false;
+  prevNext[1].disabled = false;
+});
+
+// Add event listener to the "End Button"
+endBtn.addEventListener("click", () => {
+  // Remove the "active" class from the previously active number link
+  document.querySelector(".active").classList.remove("active");
+  // Add the "active" class to the last number link
+  numbers[4].classList.add("active");
+  currentStep = 4;
+  updateBtn(); // Update button state
+  startBtn.disabled = false;
+  prevNext[0].disabled = false;
+});
